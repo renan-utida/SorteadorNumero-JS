066398a6fc3e214cb2f436841e51a5d0b38a2d5a
Renan <renandiutida@gmail.com>
2026-02-02 19:43:33 -0300

    Comecando a criar um projeto de sorteador de numeros, criamos uma funcao para sortear um numero aleatorio

---
+++ app.js
@@ -0,0 +1,12 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let numero = obterNumeroAleatorio(de, ate);
+    alert(numero);
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
