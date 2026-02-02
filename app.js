b84c04aedf084233bdebe9eb9afe247358371478
Renan <renandiutida@gmail.com>
2026-02-02 20:58:50 -0300

    Exibindo os numeros sorteados

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
-    let numero = obterNumeroAleatorio(de, ate);
-    alert(numero);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 }
 
 function obterNumeroAleatorio(min, max){
